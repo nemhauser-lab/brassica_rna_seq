28e735a389ede0b3f265d5d20fdb6af5749754f7
morgan <morganohamm@gmail.com>
2019-04-04 15:22:26 -0700

    new comparison to Law 2018 data, this time using the raw data from them, running their analysis, to create their DEG lists from Fig. 2B of their papaer.

---
+++ code/law2018_compar_2.R
@@ -0,0 +1,183 @@
+
+# note: much of this code is taken directly from the Law 2018 supplemental methods
+
+library(rprojroot)
+library(pd.aragene.1.1.st)
+suppressPackageStartupMessages(library(oligo))
+suppressPackageStartupMessages(library(limma))
+suppressPackageStartupMessages(library(scatterplot3d))
+suppressPackageStartupMessages(library(vcd))
+suppressPackageStartupMessages(library(vsn))
+suppressPackageStartupMessages(library(DESeq))
+suppressPackageStartupMessages(library(LSD))
+suppressPackageStartupMessages(library(RColorBrewer))
+suppressPackageStartupMessages(library(VennDiagram))
+cols <- brewer.pal(8,"Dark2")
+
+
+
+rootDir <- find_root(is_rstudio_project)
+law_data_path <- file.path(rootDir, "data", "Law_et_al_2018", "CEL_files")
+celfiles <- list.celfiles(path=law_data_path, full.names=TRUE)
+rawdata<-read.celfiles(files=celfiles)
+
+pms <- pm(rawdata)
+pms<-vsn::vsnMatrix(pms)
+meanSdPlot(pms)
+
+pm(rawdata) <- exprs(pms)
+eset <- rma(rawdata,normalize=FALSE,background=FALSE)
+
+# annotation
+annot <- pData(getNetAffx(eset,'transcript'))
+stopifnot(all(rownames(exprs(eset)) == rownames(annot)))
+sel <- rowSums(is.na(annot))==18
+annot <- annot[!sel,]
+atg <- regexpr("at\\d{1}g\\d{5}",annot[,"mrnaassignment"],ignore.case=TRUE)
+ID <- substr(annot[,"mrnaassignment"],atg,atg+attr(atg,"match.length")-1)
+annot$ID <- ID
+
+# "between array normalization"
+n.mat <- 2^exprs(eset)[!sel,]
+rownames(n.mat)[-which(is.na(ID) | ID =="")] <- ID[-which(is.na(ID) | ID =="")]
+colnames(n.mat) <- sub("_Ara.*","",colnames(n.mat))
+mat <- normalizeBetweenArrays(n.mat,method="cyclicloess")
+
+# remove duplicated genes
+sprintf("There are %s duplicated genes (i.e. 2 or more probes per gene)",sum(duplicated(rownames(mat))))
+sel <- rownames(mat) %in% rownames(mat)[duplicated(rownames(mat))]
+print("Number of probes per gene for these genes:")
+table(table(rownames(mat)[sel]))
+
+mat <- rbind(mat[!sel,],
+             t(sapply(unique(rownames(mat)[duplicated(rownames(mat))]),
+                      function(nam,mat){
+                        lmat <- mat[rownames(mat) %in% nam,]
+                        lmat[ rowMeans(lmat) == max(rowMeans(lmat)), ,drop=FALSE][1,]
+                      },mat)))
+mat <- mat[grepl("A[t,T]",rownames(mat)),]
+mat <- mat[order(rownames(mat)),]
+
+# quality control (just to see if I have the same data they do)
+par(mar=c(6.1,4.1,4.1,2.1))
+boxplot(mat,las=2,main="normalized")
+
+# DEG analysis
+# design
+sample.type <- sub("Wt","IDL",sub("Wt-t0","t0",gsub("_Rep\\d","",colnames(mat))))
+sample.type <- factor(sub("^.*_", "", sample.type)) # removes GSE..._
+levels(sample.type) <- sub("-",".",levels(sample.type))
+design <- model.matrix(~0+sample.type)
+colnames(design) <- levels(sample.type)
+fit <- lmFit(mat,design)
+
+# model fit DP.t3 vs. IDL.t3 (should get 3819 DEGs)
+count.matrix <- makeContrasts(T3=DP.t3-IDL.t3,levels=design)
+fit2 <- contrasts.fit(fit,count.matrix)
+fit2 <- eBayes(fit2)
+res <- topTable(fit2,number=nrow(mat))
+sum(res$adj.P.Val <= 0.01) # should get 3819 DEGs
+
+#===============================================================================
+# Data from our results, run first part of DGE_analysis_v3.rmd first
+
+# WT S vs P
+SvP_DF <- allGenesWT_SvP
+SvP_DF$Gene_ID <- row.names(SvP_DF)
+SvP_DF <- dplyr::left_join(SvP_DF, edinburghGeneInfo, by="Gene_ID")
+
+
+#===============================================================================
+#Comparisons
+
+
+
+
+# ---------------------------------
+# D3
+count.matrix <- makeContrasts(DP_D3=DP.t3-t0,levels=design)
+fit2 <- contrasts.fit(fit,count.matrix)
+fit2 <- eBayes(fit2)
+DP_res <- topTable(fit2,number=nrow(mat))
+sum(DP_res$adj.P.Val <= 0.01) # should be 2826 + 3968 + 1001 + 899 = 8694 DEGs
+DP_res$At_Gene_ID <- row.names(DP_res)
+
+count.matrix <- makeContrasts(IDL_D3=IDL.t3-t0,levels=design)
+fit2 <- contrasts.fit(fit,count.matrix)
+fit2 <- eBayes(fit2)
+IDL_res <- topTable(fit2,number=nrow(mat))
+sum(IDL_res$adj.P.Val <= 0.01) # should be 2826 + 3968 + 1793 + 1104 = 9691 DEGs
+IDL_res$At_Gene_ID <- row.names(IDL_res)
+
+res <- dplyr::inner_join(DP_res, IDL_res, by="At_Gene_ID")
+colnames(res) <- sub("\\.x", ".DP", colnames(res))
+colnames(res) <- sub("\\.y", ".IDL", colnames(res))
+
+# recreate T3  venn diagram from Law fig. 2B
+resVennDF <- data.frame("DP.t3" = with(res, (adj.P.Val.DP <= 0.01)*sign(logFC.DP)),
+                        "IDL.t3" = with(res, (adj.P.Val.IDL <= 0.01)*sign(logFC.IDL)),
+                        row.names = res$At_Gene_ID)
+#vennDiagram(resVennDF, include=c("up","down"))
+
+CommonDF <- dplyr::inner_join(res, SvP_DF[,c(1:8)], by=c("At_Gene_ID" = "A._thaliana_best_hit"))
+
+nrow(subset(CommonDF, abs(logFC)>=1 & FDR<=0.01))
+nrow(subset(CommonDF, adj.P.Val.DP <= 0.01 & adj.P.Val.IDL <= 0.01 ))
+
+vennDF <- data.frame("SvP" = with(CommonDF, (abs(logFC) >= 1 & FDR <= 0.01) * sign(logFC)),
+                     "DP.t3" = with(CommonDF, (adj.P.Val.DP <= 0.01) * sign(logFC.DP)),
+                     "IDL.t3" = with(CommonDF, (adj.P.Val.IDL <= 0.01) * sign(logFC.IDL)))
+vennDiagram(vennDF, include=c("up","down"))
+vennDiagram(vennDF, include="both")
+
+# ---------------------------------------
+# D6
+
+count.matrix <- makeContrasts(DP_D6=DP.t6-t0,levels=design)
+fit2 <- contrasts.fit(fit,count.matrix)
+fit2 <- eBayes(fit2)
+DP_res <- topTable(fit2,number=nrow(mat))
+sum(DP_res$adj.P.Val <= 0.01) # should be 2714 + 3963 + 1708 + 1422 = 9807 DEGs
+DP_res$At_Gene_ID <- row.names(DP_res)
+
+count.matrix <- makeContrasts(IDL_D6=IDL.t6-t0,levels=design)
+fit2 <- contrasts.fit(fit,count.matrix)
+fit2 <- eBayes(fit2)
+IDL_res <- topTable(fit2,number=nrow(mat))
+sum(IDL_res$adj.P.Val <= 0.01) # should be 2714 + 3963 + 2313 + 1270 = 10260 DEGs
+IDL_res$At_Gene_ID <- row.names(IDL_res)
+
+res <- dplyr::inner_join(DP_res, IDL_res, by="At_Gene_ID")
+colnames(res) <- sub("\\.x", ".DP", colnames(res))
+colnames(res) <- sub("\\.y", ".IDL", colnames(res))
+
+# recreate T6 venn diagram from Law fig. 2B
+resVennDF <- data.frame("DP.t6" = with(res, (adj.P.Val.DP <= 0.01)*sign(logFC.DP)),
+                        "IDL.t6" = with(res, (adj.P.Val.IDL <= 0.01)*sign(logFC.IDL)),
+                        row.names = res$At_Gene_ID)
+colnames
+vennDiagram(resVennDF, include=c("up","down"))
+
+CommonDF <- dplyr::inner_join(res, SvP_DF[,c(1:8)], by=c("At_Gene_ID" = "A._thaliana_best_hit"))
+
+
+vennDF <- data.frame("SvP" = with(CommonDF, (abs(logFC)>1 & FDR<0.01)*sign(logFC)),
+                     "DP.t6" = with(CommonDF, (adj.P.Val.DP <= 0.01)*sign(logFC.DP)),
+                     "IDL.t6" = with(CommonDF, (adj.P.Val.IDL <= 0.01)*sign(logFC.IDL)))
+vennDiagram(vennDF, include=c("up","down"))
+vennDiagram(vennDF, include="both")
+title(list("title here", font=1, cex=2),  line=0)
+
+
+temp <- aaply(t(vennDF), .margins=2,  paste, collapse="", .expand=FALSE)
+
+temp <- vennDF$SvP!=-1 & vennDF$DP.t3!=-1 & vennDF$IDL.t3!=-1
+sum(temp)
+length(unique(CommonDF$At_Gene_ID[temp]))
+
+# coppy list of unique At_Gene_IDs to clipboard for specific condition
+temp <- vennDF$SvP!=-1 & vennDF$DP.t6==-1 & vennDF$IDL.t6==-1
+sum(temp)
+clipr::write_clip(unique(CommonDF$At_Gene_ID[temp]))
+
+clipr::write_clip(CommonDF)
--- code/law2018_comparison.R
+++ code/law2018_comparison.R
@@ -1,117 +1,126 @@
 # comparison to Law et. al. 2018
 # NOTE: this script references lists from analysis_v3, run that file first.
 
-library(clipr)
+library(rprojroot)
 library(openxlsx)
+
+rootDir <- find_root(is_rstudio_project)
 lawS1Path <- file.path(rootDir, "data", "Law_et_al_2018",
                        "PP2018-RA-00062DR1_Supplemental_Table_1.xlsx")
 
+# getting the data -------------------------------------------------------------
+
 df <- read.xlsx(xlsxFile=lawS1Path, sheet=1, skipEmptyRows=FALSE, startRow=2)
 col <- colnames(df)
-col <- gsub(".-.", "_", col)
+col <- gsub("\\.-\\.", "_", col)
 col <- gsub("\\.", "_", col)
 colnames(df) <- col
-temp3 <- df
-
-# # take data from clipboard and parse
-# temp <- clipr::read_clip()
-# temp2 <- stringr::str_split(temp, "\t")
-# temp3 <- data.frame(do.call(rbind, temp2 ), stringsAsFactors=FALSE)
-# cols <- temp3[1, ]
-# cols <- gsub(" - ", "_", cols)
-# cols <- gsub(" ", "_", cols)
-# colnames(temp3) <- cols
-# temp3 <- temp3[-1, ]
-# # convert character columns to numeric
-# temp3$T0 <- as.numeric(temp3$T0)
-# temp3$DP_D3 <- as.numeric(temp3$DP_D3)
-# temp3$DP_D6 <- as.numeric(temp3$DP_D6)
-# temp3$IDL_D3 <- as.numeric(temp3$IDL_D3)
-# temp3$IDL_D6 <- as.numeric(temp3$IDL_D6)
+lawDF <- df
 
 # calculate fold changes
-temp3$idl_d3_vs_d0 <- log(temp3$IDL_D3/temp3$T0, 2)
-temp3$idl_d6_vs_d0 <- log(temp3$IDL_D6/temp3$T0, 2)
-temp3$dp_d3_vs_d0 <- log(temp3$DP_D3/temp3$T0, 2)
-temp3$dp_d6_vs_d0 <- log(temp3$DP_D6/temp3$T0, 2)
+lawDF$IDL_D3_vs_D0 <- log(lawDF$IDL_D3/lawDF$T0, 2)
+lawDF$IDL_D6_vs_D0 <- log(lawDF$IDL_D6/lawDF$T0, 2)
+lawDF$DP_D3_vs_D0 <- log(lawDF$DP_D3/lawDF$T0, 2)
+lawDF$DP_D6_vs_D0 <- log(lawDF$DP_D6/lawDF$T0, 2)
 
+# WT S vs P
+SvP_DF <- allGenesWT_SvP
+SvP_DF$Gene_ID <- row.names(SvP_DF)
+SvP_DF <- dplyr::left_join(SvP_DF, edinburghGeneInfo, by="Gene_ID")
 
-temp4 <- allGenesWT_SvP
-temp4$Gene_ID <- row.names(temp4)
-temp4 <- dplyr::left_join(temp4, edinburghGeneInfo, by="Gene_ID")
+commonGenes <- lawDF[lawDF$Locus_ID %in% SvP_DF$A._thaliana_best_hit, "Locus_ID"]
 
-commonGenes <- temp3[temp3$Locus_ID %in% temp4$A._thaliana_best_hit, "Locus_ID"]
-
-temp5 <- dplyr::left_join(temp3, temp4[,c(1:8)], by=c("Locus_ID" = "A._thaliana_best_hit"))
-
-sum(is.na(temp5$idl_d3_vs_d0))
-temp5 <- temp5[!is.na(temp5$idl_d3_vs_d0), ]
-temp5 <- temp5[!is.na(temp5$logFC), ]
+# combined dataframe
+CommonDF <- dplyr::left_join(lawDF, SvP_DF[,c(1:8)], by=c("Locus_ID" = "A._thaliana_best_hit"))
+# sum(is.na(CommonDF$IDL_D3_vs_D0))
+CommonDF <- CommonDF[!is.na(CommonDF$IDL_D3_vs_D0), ]
+CommonDF <- CommonDF[!is.na(CommonDF$logFC), ]
+CommonDF$logFC_WT <- CommonDF$logFC
+CommonDF$logFC_phyB <- allGenesphyB_SvP[CommonDF$Gene_ID, "logFC"]
 
 # standard deviations of idl and dp columns
-sdD3Idl <- sd(temp5$idl_d3_vs_d0)
-sdD6Idl <- sd(temp5$idl_d6_vs_d0)
-sdD3Dp <- sd(temp5$dp_d3_vs_d0)
-sdD6Dp <- sd(temp5$dp_d6_vs_d0)
+sdD3Idl <- sd(CommonDF$IDL_D3_vs_D0)
+sdD6Idl <- sd(CommonDF$IDL_D6_vs_D0)
+sdD3Dp <- sd(CommonDF$DP_D3_vs_D0)
+sdD6Dp <- sd(CommonDF$DP_D6_vs_D0)
 
-# Analysis below -------------------------------------------
+# Analysis/comparisons ---------------------------------------------------------
 
-ggplot(temp5, aes(x=idl_d3_vs_d0, y=logFC)) + geom_point(stroke=0, alpha=0.5)
-ggplot(temp5, aes(x=idl_d6_vs_d0, y=logFC)) + geom_point(stroke=0, alpha=0.5)
-ggplot(temp5, aes(x=idl_d3_vs_d0, y=idl_d6_vs_d0)) + geom_point(stroke=0, alpha=0.5)
+ggplot(CommonDF, aes(x=IDL_D3_vs_D0, y=logFC_WT)) + geom_point(stroke=0, alpha=0.5)
+ggplot(CommonDF, aes(x=IDL_D6_vs_D0, y=logFC_WT)) + geom_point(stroke=0, alpha=0.5)
+ggplot(CommonDF, aes(x=IDL_D3_vs_D0, y=IDL_D6_vs_D0)) + geom_point(stroke=0, alpha=0.5)
 
-cor(temp5[, c("idl_d3_vs_d0",
-              "idl_d6_vs_d0",
-              "dp_d3_vs_d0",
-              "dp_d6_vs_d0",
-              "logFC")], method="pearson")
+# correlation between log fold changes in different sets.
 
-cor(temp5[, c("idl_d3_vs_d0",
-              "idl_d6_vs_d0",
-              "dp_d3_vs_d0",
-              "dp_d6_vs_d0",
-              "logFC")], method="spearman")
+cor(CommonDF[, c("IDL_D3_vs_D0",
+              "IDL_D6_vs_D0",
+              "DP_D3_vs_D0",
+              "DP_D6_vs_D0",
+              "logFC_WT")], method="pearson")
+
+cor(CommonDF[, c("IDL_D3_vs_D0",
+              "IDL_D6_vs_D0",
+              "DP_D3_vs_D0",
+              "DP_D6_vs_D0",
+              "logFC_WT")], method="spearman")
 
 
-ggplot(temp5, aes(x=dp_d3_vs_d0, y=logFC)) + geom_point(stroke=0, alpha=0.5)
-ggplot(temp5, aes(x=dp_d6_vs_d0, y=logFC)) + geom_point(stroke=0, alpha=0.5)
-ggplot(temp5, aes(x=dp_d3_vs_d0, y=dp_d6_vs_d0)) + geom_point(stroke=0, alpha=0.5)
+ggplot(CommonDF, aes(x=DP_D3_vs_D0, y=logFC_WT)) + geom_point(stroke=0, alpha=0.5)
+ggplot(CommonDF, aes(x=DP_D6_vs_D0, y=logFC_WT)) + geom_point(stroke=0, alpha=0.5)
+ggplot(CommonDF, aes(x=DP_D3_vs_D0, y=DP_D6_vs_D0)) + geom_point(stroke=0, alpha=0.5)
 
-ggplot(temp5, aes(x=idl_d6_vs_d0, y=dp_d6_vs_d0)) + geom_point(stroke=0, alpha=0.5)
+ggplot(CommonDF, aes(x=IDL_D6_vs_D0, y=DP_D6_vs_D0)) + geom_point(stroke=0, alpha=0.5)
 
-ggplot(temp3, aes(x=idl_d3_vs_d0, y=idl_d6_vs_d0)) + geom_point()
-plot(temp5$idl_d3_vs_d0, temp5$logFC)
+ggplot(lawDF, aes(x=IDL_D3_vs_D0, y=IDL_D6_vs_D0)) + geom_point()
+plot(CommonDF$IDL_D3_vs_D0, CommonDF$logFC)
 
-nrow(subset(temp4, FDR<0.01 & abs(logFC)>1 & (A._thaliana_best_hit %in% commonGenes)
+nrow(subset(SvP_DF, FDR<0.01 & abs(logFC)>1 & (A._thaliana_best_hit %in% commonGenes)
             , select=A._thaliana_best_hit))
 
 
-sum(temp3$Locus_ID[1:1000] %in% subset(temp4, FDR<0.01&abs(logFC)>1)$A._thaliana_best_hit)
+sum(lawDF$Locus_ID[1:1000] %in% subset(SvP_DF, FDR<0.01&abs(logFC)>1)$A._thaliana_best_hit)
 
-nrow(subset(temp5, FDR<0.01 & abs(logFC)>1))
-nrow(subset(temp5, abs(idl_d3_vs_d0)>2))
-nrow(subset(temp5, FDR<0.01 & abs(logFC)>1 & abs(idl_d3_vs_d0)>2))
+nrow(subset(CommonDF, FDR<0.01 & abs(logFC)>1))
+nrow(subset(CommonDF, abs(IDL_D3_vs_D0)>2))
+nrow(subset(CommonDF, FDR<0.01 & abs(logFC)>1 & abs(IDL_D3_vs_D0)>2))
 
-sdD3Idl <- sd(temp5$idl_d3_vs_d0)
-sdD6Idl <- sd(temp5$idl_d6_vs_d0)
-sdD3Dp <- sd(temp5$dp_d3_vs_d0)
-sdD6Dp <- sd(temp5$dp_d6_vs_d0)
+sdD3Idl <- sd(CommonDF$IDL_D3_vs_D0)
+sdD6Idl <- sd(CommonDF$IDL_D6_vs_D0)
+sdD3Dp <- sd(CommonDF$DP_D3_vs_D0)
+sdD6Dp <- sd(CommonDF$DP_D6_vs_D0)
+
+
+length(unique(CommonDF$Locus_ID))
+length(unique(subset(CommonDF, FDR<2 & abs(logFC)>1)$Locus_ID))
+
 
 # D3 vs D0 IDL only
-nrow(subset(temp5, abs(idl_d3_vs_d0)>2 & abs(dp_d3_vs_d0)<2))
-nrow(subset(temp5, abs(idl_d3_vs_d0)>2 & abs(dp_d3_vs_d0)<2 & FDR<0.01 & abs(logFC)>1))
+nrow(subset(CommonDF, abs(IDL_D3_vs_D0)>2 & abs(DP_D3_vs_D0)<2))
+nrow(subset(CommonDF, abs(IDL_D3_vs_D0)>2 & abs(DP_D3_vs_D0)<2 & FDR<0.01 & abs(logFC)>1))
+
+length(unique(subset(CommonDF, abs(IDL_D3_vs_D0)>1 & abs(DP_D3_vs_D0)<1)$Locus_ID))
+length(unique(subset(CommonDF, abs(IDL_D3_vs_D0)>1 & abs(DP_D3_vs_D0)<1 & FDR<2 & abs(logFC)>1)$Locus_ID))
 
 # D3 vs D0 DP only
-nrow(subset(temp5, abs(dp_d3_vs_d0)>2 & abs(idl_d3_vs_d0)<2))
-nrow(subset(temp5, abs(dp_d3_vs_d0)>2 & abs(idl_d3_vs_d0)<2 & FDR<0.01 & abs(logFC)>1))
+nrow(subset(CommonDF, abs(DP_D3_vs_D0)>2 & abs(IDL_D3_vs_D0)<2))
+nrow(subset(CommonDF, abs(DP_D3_vs_D0)>2 & abs(IDL_D3_vs_D0)<2 & FDR<0.01 & abs(logFC)>1))
+
+length(unique(subset(CommonDF, abs(DP_D3_vs_D0)>1 & abs(IDL_D3_vs_D0)<1)$Locus_ID))
+length(unique(subset(CommonDF, abs(DP_D3_vs_D0)>1 & abs(IDL_D3_vs_D0)<1 & FDR<2 & abs(logFC)>1)$Locus_ID))
 
 # D6 vs D0 IDL only
-nrow(subset(temp5, abs(idl_d6_vs_d0)>2 & abs(dp_d6_vs_d0)<2))
-nrow(subset(temp5, abs(idl_d6_vs_d0)>2 & abs(dp_d6_vs_d0)<2 & FDR<0.01 & abs(logFC)>1))
+nrow(subset(CommonDF, abs(IDL_D6_vs_D0)>2 & abs(DP_D6_vs_D0)<2))
+nrow(subset(CommonDF, abs(IDL_D6_vs_D0)>2 & abs(DP_D6_vs_D0)<2 & FDR<0.01 & abs(logFC)>2))
+
+length(unique(subset(CommonDF, abs(IDL_D6_vs_D0)>1 & abs(DP_D6_vs_D0)<1)$Locus_ID))
+length(unique(subset(CommonDF, abs(IDL_D6_vs_D0)>1 & abs(DP_D6_vs_D0)<1 & FDR<2 & abs(logFC)>1)$Locus_ID))
 
 # D6 vs D0 DP only
-nrow(subset(temp5, abs(dp_d6_vs_d0)>2 & abs(idl_d6_vs_d0)<2))
-nrow(subset(temp5, abs(dp_d6_vs_d0)>2 & abs(idl_d6_vs_d0)<2 & FDR<0.01 & abs(logFC)>1))
+nrow(subset(CommonDF, abs(DP_D6_vs_D0)>2 & abs(IDL_D6_vs_D0)<2))
+nrow(subset(CommonDF, abs(DP_D6_vs_D0)>2 & abs(IDL_D6_vs_D0)<2 & FDR<0.01 & abs(logFC)>1))
+
+length(unique(subset(CommonDF, abs(DP_D6_vs_D0)>1 & abs(IDL_D6_vs_D0)<1)$Locus_ID))
+length(unique(subset(CommonDF, abs(DP_D6_vs_D0)>1 & abs(IDL_D6_vs_D0)<1 & FDR<2 & abs(logFC)>1)$Locus_ID))
 
 
 # ------------------------------------------------------------------------------
@@ -132,7 +141,7 @@ df$litSign <- litValSign
 
 rownames(df) <- df$AGI
 
-temp6 <- subset(temp5, Locus_ID %in% df$AGI)
+temp6 <- subset(CommonDF, Locus_ID %in% df$AGI)
 
 temp6$litSign <- df[temp6$Locus_ID, "litSign"]
 temp6$logFC_phyB <- allGenesphyB_SvP[temp6$Gene_ID, "logFC"]
@@ -148,10 +157,11 @@ sum(temp6$litSign * temp6$logFC > 0)/nrow(temp6)
 sum(temp6$litSign * temp6$logFC_phyB > 0)/nrow(temp6)
 # 0.7347
 
+# binomial significance test
+binom.test(32,49,0.5,alternative="greater")
 
-
-sum(temp6$dp_d6_vs_d0 * temp6$litSign > 0)
-sum(temp6$idl_d6_vs_d0 * temp6$litSign > 0)
+sum(temp6$DP_D6_vs_D0 * temp6$litSign > 0)
+sum(temp6$IDL_D6_vs_D0 * temp6$litSign > 0)
 
 
 
